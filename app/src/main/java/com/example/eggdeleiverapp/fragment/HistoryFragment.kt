6c43fb2e9afedc93883a519d52c896184d7f6a47
msi_PC <babdiyev361@gmail.com>
2022-05-13 18:07:39 +0500

    view works new

--- app/src/main/java/com/example/eggdeleiverapp/fragment/HistoryFragment.kt
+++ app/src/main/java/com/example/eggdeleiverapp/fragment/HistoryFragment.kt
@@ -6,21 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.eggdeleiverapp.R
+import com.example.eggdeleiverapp.common.Constant.HISTORY
 import com.example.eggdeleiverapp.fragment.adapter.HistoryAdapter
 import com.example.eggdeleiverapp.fragment.adapter.HistoryItem
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_history.view.*
-import java.util.*
 
 class HistoryFragment : Fragment() {
+    private lateinit var mAuth: FirebaseAuth
     private lateinit var adapter: HistoryAdapter
+    private val mDatabase = FirebaseDatabase.getInstance().reference
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        val list =
-            mutableListOf(HistoryItem("ALi", Date(System.currentTimeMillis()), null, "+sdfas", 5))
-            adapter = HistoryAdapter(list, context)
+        mAuth = FirebaseAuth.getInstance()
+        //todo add right to left animation
+
+        val query = mDatabase
+            .child(HISTORY)
+            .child(mAuth.currentUser!!.uid)
+
+        val options: FirebaseRecyclerOptions<HistoryItem> =
+            FirebaseRecyclerOptions.Builder<HistoryItem>()
+                .setQuery(query, HistoryItem::class.java)
+                .build()
+
+        adapter = HistoryAdapter(options)
+
         val view = inflater.inflate(R.layout.fragment_history, container, false)
         view.rv_history.adapter = adapter
         return view
--- app/src/main/java/com/example/eggdeleiverapp/fragment/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/example/eggdeleiverapp/fragment/adapter/HistoryAdapter.kt
@@ -1,28 +1,30 @@
 package com.example.eggdeleiverapp.fragment.adapter
 
 import android.annotation.SuppressLint
-import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eggdeleiverapp.R
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import kotlinx.android.synthetic.main.item_view.view.*
 import java.text.SimpleDateFormat
 
-class HistoryAdapter(private val list: MutableList<HistoryItem>, private val context: Context?) :
-    RecyclerView.Adapter<HistoryAdapter.VH>() {
+class HistoryAdapter(
+    options: FirebaseRecyclerOptions<HistoryItem>
+) :
+    FirebaseRecyclerAdapter<HistoryItem, HistoryAdapter.VH>(options) {
 
+
+    override fun onBindViewHolder(holder: VH, position: Int, model: HistoryItem) {
+        holder.onBind(model)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        return VH(LayoutInflater.from(context!!).inflate(R.layout.item_view, parent, false))
+        return VH(LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false))
     }
-
-    override fun onBindViewHolder(holder: VH, position: Int) {
-        holder.onBind(list[position])
-    }
-
-    override fun getItemCount() = list.size
 
     class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -34,5 +36,4 @@ class HistoryAdapter(private val list: MutableList<HistoryItem>, private val con
             itemView.order_count.text = historyItem.count.toString()
         }
     }
-
 }
