30c9b50071ca517e218ca9bbd57307f9c8f94407
acer_PC <babdiyev361@gmail.com>
2022-05-13 05:24:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/eggdeleiverapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.eggdeleiverapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.eggdeleiverapp.reg_view.RegisterActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (auth.currentUser == null) {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "already login", Toast.LENGTH_LONG).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/eggdeleiverapp/reg_view/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.eggdeleiverapp.reg_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eggdeleiverapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+
+
+    }
+}
