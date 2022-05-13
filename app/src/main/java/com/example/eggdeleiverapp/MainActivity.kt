3d9407e5c306abe095d65485d05d3e7619dec83e
msi_PC <babdiyev361@gmail.com>
2022-05-13 18:07:39 +0500

    view works

--- app/src/main/java/com/example/eggdeleiverapp/MainActivity.kt
+++ app/src/main/java/com/example/eggdeleiverapp/MainActivity.kt
@@ -4,22 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setPadding
 import com.example.eggdeleiverapp.reg_view.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private var phone: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
+        checkHasLogin()
+
+        restAll()
+        container_order.setBackgroundColor(resources.getColor(R.color.main_color))
+        order.setPadding(7)
     }
 
-    override fun onResume() {
-        super.onResume()
-
+    private fun checkHasLogin() {
         if (auth.currentUser == null) {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
@@ -27,5 +33,13 @@ class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "already login", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun restAll() {
+        container_history.setBackgroundColor(resources.getColor(R.color.white))
+        container_order.setBackgroundColor(resources.getColor(R.color.white))
+
+        history.setPadding(15)
+        order.setPadding(15)
     }
 }
